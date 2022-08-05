19cae68f9249bb4721e60f7df8fe5d08fa91a5bf
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2022-08-06 00:31:38 +0200

    Create all drivers header

---
+++ include/ps2_all_drivers.h
@@ -0,0 +1,23 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2005, ps2dev - http://www.ps2dev.org
+# Licenced under GNU Library General Public License version 2
+# Review ps2sdk README & LICENSE files for further details.
+#
+# PS2_ALL_DRIVERS
+*/
+
+#ifndef PS2_ALL_DRIVERS
+#define PS2_ALL_DRIVERS
+
+#include <ps2_audio_driver.h>
+#include <ps2_fileXio_driver.h>
+#include <ps2_joystick_driver.h>
+#include <ps2_memcard_driver.h>
+#include <ps2_sio2man_driver.h>
+#include <ps2_usb_driver.h>
+
+#endif /* PS2_ALL_DRIVERS */
