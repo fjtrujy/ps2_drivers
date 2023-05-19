2cca67c68d699b157f12f7821f5943f8a38c8b66
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2023-05-19 20:35:21 +0200

    Adding the USB Mouse driver

---
+++ include/ps2_mouse_driver.h
@@ -0,0 +1,39 @@
+/*
+# _____     ___ ____     ___ ____
+#  ____|   |    ____|   |        | |____|
+# |     ___|   |____ ___|    ____| |    \    PS2DEV Open Source Project.
+#-----------------------------------------------------------------------
+# Copyright 2005, ps2dev - http://www.ps2dev.org
+# Licenced under GNU Library General Public License version 2
+# Review ps2sdk README & LICENSE files for further details.
+#
+# PS2_MOUSE_DRIVER
+*/
+
+#ifndef PS2_MOUSE_DRIVER
+#define PS2_MOUSE_DRIVER
+
+#include <stdbool.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+enum MOUSE_INIT_STATUS {
+    MOUSE_INIT_STATUS_DEPENDENCY_ERROR = -4,
+    MOUSE_INIT_STATUS_IRX_ERROR = -3,
+    MOUSE_INIT_STATUS_EERPC_ERROR = -2,
+    MOUSE_INIT_STATUS_UNKNOWN = -1,
+    MOUSE_INIT_STATUS_OK = 0,
+    MOUSE_INIT_STATUS_IRX_OK = 1,
+};
+
+/* MOUSE DRIVER REQUIRES USBD DRIVER AS DEPENDENCY */
+enum MOUSE_INIT_STATUS init_mouse_driver(bool init_dependencies);
+void deinit_mouse_driver(bool deinit_dependencies);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PS2_MOUSE_DRIVER */
