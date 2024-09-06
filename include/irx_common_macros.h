0ac6f71eb9e6cdd07e3281ee00a809608ed71f19
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2024-09-06 13:28:44 +0200

    Fix condition to check if module can be unloaded

--- include/irx_common_macros.h
+++ include/irx_common_macros.h
@@ -15,14 +15,21 @@
     __##irx##_id = -1; \
     __##irx##_ret = -1
 
+/* Values taken from PS2SDK
+* MODULE_RESIDENT_END		0
+* MODULE_NO_RESIDENT_END	1
+* MODULE_REMOVABLE_END	2 // Only for 'modload' > v1.2
+*/
+
 /// condition to perform irx load
 #define CHECK_IRX_LOAD(irx) (__##irx##_id == -1) && (__##irx##_ret == -1)
 
 /// condition for irx startup errors
 #define CHECK_IRX_ERR(irx) (__##irx##_id < 0 || __##irx##_ret == 1)
 
-/// check if this irx module is successfully loaded and supports unloading
-#define CHECK_IRX_UNLOAD(irx) (__##irx##_id >= 0 && __##irx##_ret == 2)
+/// check if this irx module is successfully loaded and supports unloading.
+/// It should use MODULE_REMOVABLE_END however in PS2SDK it is not used, so we will use MODULE_RESIDENT_END
+#define CHECK_IRX_UNLOAD(irx) (__##irx##_id >= 0 && __##irx##_ret == 0)
 /// extern an embedded irx file buffer and size
 #define EXTERN_IRX(_IRX) \
 extern unsigned char _IRX[] __attribute__((aligned(16))); \
